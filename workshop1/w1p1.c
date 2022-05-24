b12782f27087f313fdfc413f63369bbbfefa7d95
SmitPatel121 <106198185+SmitPatel121@users.noreply.github.com>
2022-05-24 18:20:41 -0400

    Add files via upload

---
+++ workshop1/w1p1.c
@@ -0,0 +1,30 @@
+/* *****************************************************************************
+                   <assessment name example: Workshop - #2 (Part-1)>
+     Full Name  : Smit anjaykumar patel
+     Student ID#: 104424213
+     Email      : sapatel59@myseneca.ca
+     Section    : IPC144NMM.10348.2221
+
+     Authenticity Declaration:
+     I declare this submission is the result of my own work and has not been
+     shared with any other student or 3rd party content provider. This submitted
+     piece of work is entirely of my own creation.
+     ***************************************************************************** */
+
+
+
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Workshop 1 Part-1\n");
+    printf("=================\n\n");
+    printf("I'm displaying this using the 'printf' stdio\n(standard input output) library function!\n\n");
+    printf("Dear teacher,\n\n  I promise I will work hard from this day onward.\n  I acknowledge that practice is extremely important,\n  so I will do all workshops, quizzes, and assignments.\n\n");
+
+    printf("Sincerely,\n\n");
+    printf("Smit Patel\n");
+    printf("Student ID# 104424213");
+	return 0;
+}
---
+++ workshop1/w1p2.c
@@ -0,0 +1,35 @@
+/* *****************************************************************************
+                   <Workshop - #1 (Part-2)>
+     Full Name  : Smit anjaykumar patel
+     Student ID#: 104424213
+     Email      : sapatel59@myseneca.ca
+     Section    : IPC144NMM.10348.2221
+
+     Authenticity Declaration:
+     I declare this submission is the result of my own work and has not been
+     shared with any other student or 3rd party content provider. This submitted
+     piece of work is entirely of my own creation.
+     ***************************************************************************** */
+
+
+
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("Workshop 1 Part-2\n");
+    printf("=================\n\n");
+    printf("Using tab specifiers...\n");
+    printf("\tTab-1\tTab-2\tTab-3\n\t-----\t=====\t-----\n\n");
+
+    printf("\\ this is a back-slash character!\n");
+    printf("%% this is a percent sign character!\n\" this is a double-quote character!\n\n");
+    printf("My favourite quotes are:\n1.	\"Skill is only developed by hours and hours of work.\"\n\t\t\t\t\t\t-Usain Bolt\n\n");
+    printf("2.	\"It's not about having time. It's about making time.\"\n\t\t\t\t\t\t-unknown\n\n");
+
+    printf("3.	\"All of us do not have equal talent. But, all of us\n\thave an equal opportunity to develop our talents.\"\n");
+    printf("\t\t\t\t\t\t-A.P.J. Abdul Kalam\n");	
+
+	return 0;
+}
